3f6acef48244363eb3214e4c9085eb1633775f37
Chavah Jacobs <chavah.jacobs@greensmoke.com>
2015-11-25 17:53:17 +0200

    Trying to fix update function

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-frontend-nanodegree-arcade-game
-===============================
+Help the Princess Avoid the Bugs
 
-Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
+WARNING: This game is very boring.  There is no score. There are no sounds. There is no objective. The princess must avoid the bugs, get to the water, and then start over again.
 
-for self-checking their submission.
+Instructions:
+
+Move the princess up (), down (), left (), and right ().
+
+She needs to get to the water. Why? I don't know. She just needs it.
+
+She also must avoid the bugs. Why? Because she doesn't like bugs.
+
+If she hits a bug, she freaks out and jumps back to her starting point.
--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ var Enemy = function(x, y) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    this.speed += speed * dt;
+    this.speed += 101 * dt;
 };
 
 Enemy.prototype.render = function() {
@@ -17,53 +17,58 @@ Enemy.prototype.render = function() {
 };
 
 //PLAYER
-var Player = function (x, y) {
+var Player = function(x, y) {
     this.sprite = 'images/char-princess-girl.png';
     this.x = x;
     this.y = y;
 };
 
 Player.prototype.update = function(dt) {
-    //add code here
+    if (this.y <= 0) {
+        resetPlayer();
+    } else (enemy.y === player.y && enemy.x === player.x) {
+        resetPlayer();
+    }
 };
 
 Player.prototype.render = function() {
     ctx.drawImage(resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function(keyup) {
-    if (keyup = 'left') {
-        this.x -= 101;
-    }
+resetPlayer = function() {
+    player.x = 202 && player.y = 415;
+}
 
-    if (keyup = 'up') {
-        this.y += 83;
-    }
+//https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Statements/switch
+Player.prototype.handleInput = function(location) {
+    switch (location) {
+        case 'left':
+            this.x -= 101;
+            if (this.x <= 0) {
+                this.x = 0;
+            }
+        break;
 
-    if (keyup = 'right') {
-        this.x += 83;
-    }
+        case 'up':
+            this.y -= 83;
+            if (this.y <= 0) {
+                this.y = 0;
+            }
+        break;
 
-    if (keyup = 'down') {
-        this.y -= 101;
-    }
+        case 'right':
+            this.x += 101;
+            if (this.x >= 505) {
+                this.x = 505;
+            }
+        break;
 
-    //BOUNDRIES
-
-    if (this.x <= 0) {
-        this.x = 0;
-    }
-
-    if (this.x >= 505) {
-        this.x = 505;
-    }
-
-    if (this.y <= 0) {
-        this.y = 0;
-    }
-
-    if (this.y >= 498) {
-        this.y = 498
+        case 'down':
+            this.y += 83;
+            if (this.y >= 498) {
+                this.y = 498
+            }
+        break;
     }
 };
 
@@ -72,8 +77,8 @@ Player.prototype.handleInput = function(keyup) {
 // Place the player object in a variable called player
 
 var enemy1 = new Enemy(0, 83);
-var enemy2 = new Enemy(0, 166);
-var enemy3 = new Enemy(0, 249);
+var enemy2 = new Enemy(101, 166);
+var enemy3 = new Enemy(202, 249);
 
 allEnemies = [enemy1, enemy2, enemy3];
 
@@ -82,7 +87,7 @@ var player = new Player(202, 415);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keypress', function(event) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -90,5 +95,5 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput(allowedKeys[event.keyCode]);
 });
